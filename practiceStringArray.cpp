8f490d00bbdc9672267ef498efa92eb72e796f24
Bill Nye The Russian Spy <30507350+BillNye66@users.noreply.github.com>
2018-05-11 10:39:17 -0600

    Add files via upload

--- practiceStringArray.cpp
+++ practiceStringArray.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include <typeinfo>
 #include <string>
+
 using namespace std;
 int main(int argc, char *argv[]) {
 
@@ -18,20 +19,32 @@ int main(int argc, char *argv[]) {
 	while (accepted == true) {
 		if (dataType == "short") {
 			short array[5] = {0, 0, 0, 0, 0};
-			cout << "Enter short values for the array (you will be prompted to enter five):";
+			cout << "Enter short values for the array (you will be prompted to enter five):" << endl;
 			for (int i = 0; i < 5; i++) {
 				cout << "Enter a short value for index " << i << ": ";
-				cin >> array[i];
-				cout << endl;
-			}
+				cin >> input;
+				if (input == 0) {
+					cout << endl << "Error: Data type of input does not match selected type" << endl;
+					i = i - 1;
+				}
+				else {
+					input >> array[i];		
+				}
+			}	
 		}
 		else if (dataType == "int") {
 			int array[5] = {0, 0, 0, 0, 0};
 			cout << "Enter integer values for the array (you will be prompted to enter five):";
 			for (int i = 0; i < 5; i++) {
 				cout << "Enter an integer value for index " << i << ": ";
-				cin >> array[i];
-				cout << endl;
+				cin >> input;
+				if (input == 0) {
+					cout << endl << "Error: Data type of input does not match selected type" << endl;
+					i = i - 1;
+				}
+				else {
+					input >> array[i];		
+				}
 			}
 		}
 		else if (dataType == "float") {
@@ -39,18 +52,29 @@ int main(int argc, char *argv[]) {
 			cout << "Enter float values for the array (you will be prompted to enter five):";
 			for (int i = 0; i < 5; i++) {
 				cout << "Enter a float value for index " << i << ": ";
-				cin >> array[i];
-				cout << endl;
+				cin >> input;
+				if (input == 0) {
+					cout << endl << "Error: Data type of input does not match selected type" << endl;
+					i = i - 1;
+				}
+				else {
+					input >> array[i];		
+				}
 			}
-			
 		}
 		else if (dataType == "double") {
 			double array[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
 			cout << "Enter double values for the array (you will be prompted to enter five):";
 			for (int i = 0; i < 5; i++) {
 				cout << "Enter a double value for index " << i << ": ";
-				cin >> array[i];
-				cout << endl;
+				cin >> input;
+				if (input == 0) {
+					cout << endl << "Error: Data type of input does not match selected type" << endl;
+					i = i - 1;
+				}
+				else {
+					input >> array[i];		
+				}
 			}
 			
 		}
@@ -59,20 +83,37 @@ int main(int argc, char *argv[]) {
 			cout << "Enter boolean values for the array (you will be prompted to enter five):";
 			for (int i = 0; i < 5; i++) {
 				cout << "Enter a boolean value for index " << i << ": ";
-				cin >> array[i];
-				cout << endl;
+				cin >> input;
+				if (input == 0) {
+					cout << endl << "Error: Data type of input does not match selected type" << endl;
+					i = i - 1;
+				}
+				else {
+					input >> array[i];		
+				}
 			}
-			
 		}
 		else if (dataType == "string") {
 			string array[5] = {"", "", "", "", ""};
 			cout << "Enter string values for the array (you will be prompted to enter five):";
-			
+			cin >> input;
+			if (input == 0) {
+				cout << endl << "Error: Data type of input does not match selected type" << endl;
+				i = i - 1;
+			}
+			else {
+				input >> array[i];		
+			}
 		}
 		else {
 			cout << "Input does not match a C++ datatype. Available data types are \"short\" \"integer\" \"float\" \"double\" \"boolean\" and \"string\"";
 			accepted = false;
 		}
+	}
+	cout << "You now have a " << dataType << "array with all indexes declared. With a for loop, you can sift through all of the values using the control variable of the loop." <<endl;
+	cout << "Current instruction: "
+	for (i = 0; i < 5; i++) {
+		
 	}
 	/* for (int i = 1; i < 5; i++){	cout << array[i] << endl << endl;
 		
